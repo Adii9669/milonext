cc8b29fa0fbd067d1134f24779884eb92f653792
Adii9669 <adityrajsingh799@gmail.com>
2026-08-16 12:01:00 +0530

    updated on frontend

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,12 +1,6 @@
-
-
 export * as authApi from "./auth/api";
 export * as otpApi from "./otp/api";
 export * as userApi from "./user/api";
-export * as chatApi from "./chat/api"; 
+export * as chatApi from "./chat/api";
 export * as friendApi from "./friend/api";
 export * as crewApi from "./crew/api";
-
-
-
-
